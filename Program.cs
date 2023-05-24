18aff7a23d49ad30ccd4b494df1f16796654e2ae
GiovanaVari <giovana.vari.de.assis@gmail.com>
2023-05-24 18:13:32 -0300

    LIFO

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Stack<int> pilha = new Stack<int>();
+
+pilha.Push(2);
+pilha.Push(6);
+pilha.Push(1);
+pilha.Push(4);
+pilha.Push(8);
+
+foreach (int item in pilha)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine($"Removendo o elemento do topo: {pilha.Pop()}");
+
+pilha.Push(88);
+
+foreach(int item in pilha)
+{
+    Console.WriteLine(item);
+}
